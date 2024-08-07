e36049a02979ef590b5826c8ae91d28fc3582f2c
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-08-07 23:02:53 +0530

    first commit

---
+++ Main.js
@@ -0,0 +1,87 @@
+$('#savepost').click(function () {
+    let postid =$('#postID').val()
+    let posttitle=$('#postTitle').val()
+    let posttext=$('#postText').val()
+    let postcntnt=$('#postCintent').val()
+
+    console.log(postid,posttitle,posttext,postcntnt);
+
+    $.ajax({
+        url:"http://localhost:8080/blog/savestudent",
+        method:"POST",
+        contentType:"application/json",
+            "data":JSON.stringify({
+                "id":postid,
+                "title":posttitle,
+                "text":posttext,
+                "category":postcntnt
+            }),
+        success:function (result){
+            console.log(result);
+            alert("Niceee");
+        },
+        error:function (error){
+            console.log(error);
+            alert("asfd");
+        }
+
+    })
+})
+$('#updatePost').click(function () {
+    let postid = $('#postID').val()
+    let posttitle = $('#postTitle').val()
+    let posttext = $('#postText').val()
+    let postcntnt = $('#postCintent').val()
+
+    console.log(postid, posttitle, posttext, postcntnt);
+
+    $.ajax({
+        url: "http://localhost:8080/blog/updatestudent",
+        method: "PUT",
+        contentType: "application/json",
+        "data": JSON.stringify({
+            "id": postid,
+            "title": posttitle,
+            "text": posttext,
+            "category": postcntnt
+        }),
+        success: function (result) {
+            console.log(result);
+            alert("Niceee");
+        },
+        error: function (error) {
+            console.log(error);
+            alert("OS");
+        }
+
+    })
+})
+$('#deletePost').click(function () {
+    let postid = $('#postID').val()
+    let posttitle = $('#postTitle').val()
+    let posttext = $('#postText').val()
+    let postcntnt = $('#postCintent').val()
+
+    console.log(postid, posttitle, posttext, postcntnt);
+
+    $.ajax({
+        url: "http://localhost:8080/blog/deletestudent",
+        method: "DELETE",
+        contentType: "application/json",
+        "data": JSON.stringify({
+            "id": postid,
+            "title": posttitle,
+            "text": posttext,
+            "category": postcntnt
+        }),
+        success: function (result) {
+            console.log(result);
+            alert("Niceee");
+        },
+        error: function (error) {
+            console.log(error);
+            alert("OS");
+        }
+
+    })
+})
